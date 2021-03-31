57665439ec61185c00f6764a65bb9b4379386c6c
Alex ben Hassine <alexhassine@gmail.com>
2021-03-31 17:34:48 +0200

    Fix typo's in column names. Build model. Check ICCs.

--- Ecuador Labour Project.R
+++ Ecuador Labour Project.R
@@ -1,7 +1,7 @@
 ###
 rm(list=ls())
 setwd("~/Desktop/Ecuador_labour")
-data <- readRDS("ecuador_data_2.rds")
+data <- readRDS("ecuador_data.rds")
 library(data.table)
 library(tidyverse)
 library(corrplot)
@@ -43,7 +43,7 @@ new_names <- c(
     'unemployed_pop', 'poverty', 'extr_poverty','natural_regions','stratum', 'income_pc', 'active_cellular',
     'cellular_is_smartphone','mobile_has_wifi', 'used_internet_last12months',
     'sad_due_to_LowIncome', 'sad_due_manyworkhours', 'daily_hours_internet_use', 'province_or_country',
-    'illiteracy_rate(15+)', 'average_years_of_schooling', 'hh_income_total', 'hh_size', 'hh_income_pc',
+    'illiteracy_rate(15+)', 'years_of_schooling', 'hh_income_total', 'hh_size', 'hh_income_pc',
     'economically_innactive_pop', 'economically_active_pop', 'year_of_enrollment',
     'working_equipment_in_institution_1', 'working_equipment_in_institution_2',
     'working_equipment_in_institution_3', 'working_equipment_in_institution_4',
@@ -147,6 +147,14 @@ for(i in 1:ncol(holder)){
 # noteworthy: income_labour's minimum value is -1 (?), median age is 26 yrs old, median education is "educación básica" (middle school)
 
 # Define cluster convention (Michelle)
+    #clusters
+        person #l1
+        household_id #l2
+        home_id #l3
+        conglomerado #l4
+        city #l5
+        
+    
     #socioeconomic
         income_labour
         hours_worked
@@ -162,7 +170,7 @@ for(i in 1:ncol(holder)){
         has_received_handicap_bond
         amount_handicap_bond
         enrolled_classes
-        average_years_of_schooling
+        years_of_schooling
         
     #psychosocial (preferences, subjective experience)
         job_feeling
@@ -181,23 +189,184 @@ for(i in 1:ncol(holder)){
         active_cellular
         mobile_has_wifi
         used_internet_last12months
+        area #urban vs rural, I think
+        
+    # First model attempt contains 
+        hours_worked
+        sex
+        age
+        income_labour
+        has_received_human_dev_bond
+        years_of_schooling
+        security_neighb
+        
+library(lme4)
+library(jtools)
+        
+    
+#Remove typo's 
+level_of_education_typo <- c('educaciÃ³n  media', 'educaciÃ³n bÃ¡sica', 'centro de alfabetizaciÃ³n', 'educación  media')
+level_of_education_correct <- c('educación media', 'educación básica', 'centro de alfabetización', 'educación media')
+
+dat_join <- data.table(old=level_of_education_typo, new=level_of_education_correct)
+data_transl[dat_join, on=c(level_of_education='old'), level_of_education:=new]
+
+#Add level of education as numerical variable
+level_of_education <- c('ninguno', 'centro de alfabetización', 'primaria', 'secundaria', 'educación básica', 'educación media', 'superior no universitario', 'superior universitario', 'post-grado')
+level_of_education_num <- c(0, 1, 2, 3, 4, 5, 6, 7, 8)
+
+dat_join <- data.table(level_of_education=level_of_education, level_of_education_num=level_of_education_num)
+data_transl <- data_transl[dat_join, on=c(level_of_education = 'level_of_education')]
+
+#Add job feeling as numerical variable
+job_feeling_old = c('contento', 'poco contento', 'descontento pero conforme', 
+                    'totalmente decontento', 'no sabe, no responde', NA)
+job_feeling_new = c(4, 3, 2, 1, NA, NA)
+
+data_transl[job_feeling=='no sabe, no responde', job_feeling:=rep(NA, length=.N)] 
+
+dat_join <- data.table(job_feeling = job_feeling_old, job_feeling_num = job_feeling_new)
+data_transl <- data_transl[dat_join, on=c(job_feeling='job_feeling')]
+
+## Correlating to find proxies 
+cor.test(data_transl[, years_of_schooling],
+         data_transl[, level_of_education_num], 
+         method='spearman', 
+         exact = F)
+# Correlation between years of schooling and level of education is 0.94. 
+# Will now use avg years of schooling as numerical proxy for level of education. 
+
+cor.test(data_transl[, years_of_schooling],
+         data_transl[, job_feeling_num], 
+         method='spearman', 
+         exact = F)
+# Correlation between years of schooling and job satisfaction is only 0.13. 
+# Suggests that studying well-being requires a much broader outlook 
+
+
+## Checking for ICC in three possible variables. Note: g1/g2 means g1 contains g2.
+# However, g2:g1 means g2 is contained in g1: The order switches. 
+m1 <- lmer(income_labour ~ 1 + (1|city/conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# This model adds a random effect based on city, and conglomerado per city. 
+# conglomerado per city has higher ICC (0.13), city becomes negligible (0.00)
+
+m1 <- lmer(level_of_education_num ~ 1 + (1|city/conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# This model adds a random effect based on city, and conglomerado per city. 
+# conglomerado per city has high ICC; (0,16), city remains significant (0.08)
+
+m1 <- lmer(years_of_schooling ~ 1 + (1|city/conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# This model adds a random effect based on city, and conglomerado per city. 
+# conglomerado per city has high ICC; (0,16), city is now smaller (0.01)
+# Suggests that the translation of years of schooling to level of education 
+# perhaps depends on city-specific effects? 
+
+m1 <- lmer(job_feeling_num ~ 1 + (1|city/conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# This model adds a random effect based on city, and conglomerado per city. 
+# we are finding a low correlation here for job satisfaction; 0.07 at the level 
+# of conglomerado, and only 0.02 at the level of cities. Perhaps better to skip. 
+
+## Further exploration and modeling of level of education
+m1 <- lmer(level_of_education_num ~ 1 + (1|conglomerado:city), data=data_transl, REML=FALSE)
+summ(m1)
+# Here ICC for conglomerado's as varying within cities is 0.29; some of this can 
+# be explained by inter-city variance, suggests the above model
+
+m1 <- lmer(level_of_education_num ~ 1 + (1|conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# ICC drops to 0.24 when not accounting for different conglomerado's in cities themselves. 
+
+m1 <- lmer(level_of_education_num ~ 1 + (1|city), data=data_transl, REML=FALSE)
+summ(m1)
+# Only city is 0.08. It appears conglomerado's are more important than 
+# cities. 
+
+## Further exploration and modeling of years of schooling
+m1 <- lmer(years_of_schooling ~ 1 + (1|conglomerado:city), data=data_transl, REML=FALSE)
+summ(m1)
+# Here ICC for conglomerado's as varying within cities is 0.19. This captures most
+# of the variance, it seems. 
+
+m1 <- lmer(years_of_schooling ~ 1 + (1|conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# ICC drops to 0.07 when not accounting for different conglomerado's in cities themselves. 
+
+m1 <- lmer(years_of_schooling ~ 1 + (1|city), data=data_transl, REML=FALSE)
+summ(m1)
+# Only city is 0.15. It appears conglomerado's are more important than cities, 
+# but somehow less than for level of education. This would imply that there are 
+# some cities that, with less years of schooling, achieve the same levels of 
+# education. 
+# I do not really know how to interpret the fact that, city ICC is similar to 
+# conglomerado in city ICC. 
+
+
+## I will set up the model as follows: 
+# - dependent variable: hours of schooling. This because I do not know how to 
+# correctly model categorical variables. 
+# - a random effect for individual characteristics such as intelligence.
+# - Then, I will look at each of the variables relevant for determining years of 
+# schooling: sex, age, amount_human_dev_bond to determine what cluster correlations
+# to add. 
+# - finally, I will add the relevant cluster variables. To find out what is "relevant"
+# I will compare the model at three levels (congl:city), (congl/city), (city). 
+
+### Years of schooling and age 
+m1 <- lmer(years_of_schooling ~ age + (age|city/conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# There seems to be a very low std. dev. for age varied over city/congl. Probably
+# I can leave that as a purely individual variable. 
+
+m1 <- lmer(years_of_schooling ~ age + (age|conglomerado:city), data=data_transl, REML=FALSE)
+summ(m1)
+# Again, low std. dev. for random effects for age at congl:city level. Can omit. 
+
+### Years of schooling and sex
+m1 <- lmer(years_of_schooling ~ sex + (sex|city/conglomerado), data=data_transl, REML=FALSE)
+summ(m1)
+# We find the largest effect (ICC of 0.15 and standard deviation of 1.89) at the level of 
+# cities. It appears that women in some cities perform better/worse than in others. 
+
+m1 <- lmer(years_of_schooling ~ sex + (sex|conglomerado:city), data=data_transl, REML=FALSE)
+summ(m1)
+# Lower std. dev. for random effects for sex at congl:city level (0.76, but ICC 
+# 0.17)
+
+m1 <- lmer(years_of_schooling ~ sex + (sex|city), data=data_transl, REML=FALSE)
+summ(m1)
+# Std deviation of 1.01 now. Perhaps more is absorbed by the city intercept. 
+# it is a simpler addition, so perhaps better to start with this than with 
+# city/conglomerado. 
+
+### Years of schooling and Human Dev Bond
+# Do this later. Not so relevant yet. 
+
+## The model: y = beta_0 + beta_1 age + beta_2 city + beta_3 conglomerado:city
+## + beta_4 sex:city
+m1 <- lmer(years_of_schooling ~ age + (sex|city) + (1|city/conglomerado), 
+           data=data_transl, REML=FALSE)
+
+
 
 # Visualization of distributions of relevant variables #done above
 # Intracluster correlation
-    ## Run a simple hierarchical analysis (multilevel analysis)
-        dat  <- data_transl[, c("job_feeling", "conglomerado", "hours_worked")]
-        dat2 <-aggregate(dat$hours_worked,list(dat$conglomerado),mean,na.rm=T)
-    
-        names(dat2)[names(dat2) == "Group.1"] <- "conglomerado"
-        names(dat2)[names(dat2) == "x"]       <- "hours_worked_grpMean"
-        
-        dat3 <- merge(dat, dat2, by= "conglomerado")
-        
-        dat3$job_feeling <- as.integer(factor(dat3$job_feeling))
-        
-        mod <- lm(job_feeling~hours_worked+hours_worked_grpMean, data = dat3)
-        summary(mod, cor=F)
+## Run a simple hierarchical analysis (multilevel analysis)
+dat  <- data_transl[, c("job_feeling", "conglomerado", "hours_worked")]
+dat2 <-aggregate(dat$hours_worked,list(dat$conglomerado),mean,na.rm=T)
+
+names(dat2)[names(dat2) == "Group.1"] <- "conglomerado"
+names(dat2)[names(dat2) == "x"]       <- "hours_worked_grpMean"
+
+dat3 <- merge(dat, dat2, by= "conglomerado")
+
+dat3$job_feeling <- as.integer(factor(dat3$job_feeling))
+
+mod <- lm(job_feeling~hours_worked+hours_worked_grpMean, data = dat3)
+summary(mod, cor=F)
 # if we define conglomerado as our group variable, there is a correlation of the single outcome to the mean of the group
-        #aka we do have nested data!
+#aka we do have nested data!
         
-    ## make an edgelist where 1 equals similar activitie between two agents and 0 means no similar activity
+    ## make an edgelist where 1 equals similar activitiesbetween two agents and 0 means no similar activity
